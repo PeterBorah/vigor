90f24d39521154ecff5a3c33cf5489f8fe7b17f2
Peter Borah <peterborah@gmail.com>
2013-12-12 22:37:27 +0000

    Add test for runes

--- spec/summoner_spec.rb
+++ spec/summoner_spec.rb
@@ -13,4 +13,18 @@ describe Vigor::Summoner, :vcr do
     hardiness = first_page.talents.select{|t| t.name = "Hardiness"}.first
     hardiness.id.should == 4233
   end
+
+  it "can fetch runes" do
+    vigor = Vigor::Client.new(ENV["API_KEY"])
+    summoner = vigor.summoner("semiel")
+    pages = summoner.rune_pages
+    pages.length.should == 9
+
+    current = summoner.current_rune_page
+    current.name.should == "AP/MPen/Def (AP Carry)"
+    current.should be_current
+
+    slot_1 = current.runes.select{|r| r.slot == 1}.first
+    slot_1.id.should == 5273
+  end
 end
