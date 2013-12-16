a165e59ffdb48511f0c212ee794dfe43037ed4c4
Peter Borah <peterborah@gmail.com>
2013-12-16 06:29:06 +0000

    Refactor Summoner in preparation for new API calls

--- lib/vigor/summoner.rb
+++ lib/vigor/summoner.rb
@@ -1,13 +1,19 @@
 class Vigor
   class Summoner
-    attr_accessor :id, :name, :profile_icon_id, :level, :revision_date
+    attr_accessor :id
+    Available_fields = [:name, :profile_icon_id, :level, :revision_date]
 
     def initialize(data)
+      @fields = {}
+      add_data(data)
+    end
+
+    def add_data(data)
       @id = data["id"]
-      @name = data["name"]
-      @profile_icon_id = data["profileIconId"]
-      @level = data["summonerLevel"]
-      @revision_date = DateTime.strptime(data["revisionDate"].to_s,'%s')
+      @fields[:name] = data["name"]
+      @fields[:profile_icon_id] = data["profileIconId"]
+      @fields[:level] = data["summonerLevel"]
+      @fields[:revision_date] = DateTime.strptime(data["revisionDate"].to_s,'%s')
     end
 
     def mastery_pages
@@ -24,6 +30,19 @@ class Vigor
 
     def current_rune_page
       rune_pages.find {|page| page.current? }
+    end
+
+    def method_missing(meth)
+      if Available_fields.include? meth
+        return get_or_fetch_field(meth)
+      else
+        super
+      end
+    end
+
+    def get_or_fetch_field(field)
+      add_data(Client.get("/summoner/" + @id.to_s)) unless @fields[field]
+      @fields[field]
     end
   end
 end
