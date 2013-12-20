16d9009bf44560aeb9079107b2dfa16c34a35c00
Jade McGough <jade@thezets.com>
2013-12-20 03:55:10 -0500

    Updating tests to use expect syntax

--- spec/summoner_spec.rb
+++ spec/summoner_spec.rb
@@ -6,35 +6,35 @@ describe Vigor::Summoner, :vcr do
   it "can fetch masteries" do
     summoner = Vigor.summoner("semiel")
     pages = summoner.mastery_pages
-    pages.length.should == 4
+    expect(pages.length).to eq 4
 
-    summoner.current_mastery_page.name.should == "AP"
+    expect(summoner.current_mastery_page.name).to eq "AP"
 
     first_page = pages.find{|p| p.name = "Mastery Page 1"}
-    first_page.should_not be_current
+    expect(first_page).to_not be_current
 
     hardiness = first_page.find{|t| t.name = "Hardiness"}
-    hardiness.id.should == 4233
+    expect(hardiness.id).to eq 4233
   end
 
   it "can fetch runes" do
     summoner = Vigor.summoner("semiel")
     pages = summoner.rune_pages
-    pages.length.should == 9
+    expect(pages.length).to eq 9
 
     current = summoner.current_rune_page
-    current.name.should == "AP/MPen/Def (AP Carry)"
-    current.should be_current
+    expect(current.name).to eq "AP/MPen/Def (AP Carry)"
+    expect(current).to be_current
 
     slot_1 = current.find{|r| r.slot == 1}
-    slot_1.id.should == 5273
+    expect(slot_1.id).to eq 5273
   end
 
   it "will grab extra information when needed" do
     Vigor.configure(ENV["API_KEY"])
     player = Vigor.recent_games("23893133").first.fellow_players.first
-    player.name.should == "DerpyFoo"
-    player.profile_icon_id.should == 582
-    player.team_id.should == 100
+    expect(player.name).to eq "DerpyFoo"
+    expect(player.profile_icon_id).to eq 582
+    expect(player.team_id).to eq 100
   end
 end
--- spec/vigor_spec.rb
+++ spec/vigor_spec.rb
@@ -5,74 +5,74 @@ describe Vigor, :vcr do
 
   it "can get a summoner by name" do
     summoner = Vigor.summoner("semiel")
-    summoner.id.should == 23893133
-    summoner.name.should == "Semiel"
-    summoner.profile_icon_id.should == 518
-    summoner.level.should == 30
+    expect(summoner.id).to eq 23893133
+    expect(summoner.name).to eq "Semiel"
+    expect(summoner.profile_icon_id).to eq 518
+    expect(summoner.level).to eq 30 
   end
 
   it "can get a summoner by id" do
     summoner = Vigor.summoner(23893133)
-    summoner.id.should == 23893133
-    summoner.name.should == "Semiel"
-    summoner.profile_icon_id.should == 518
-    summoner.level.should == 30
+    expect(summoner.id).to eq 23893133
+    expect(summoner.name).to eq "Semiel"
+    expect(summoner.profile_icon_id).to eq 518
+    expect(summoner.level).to eq 30
   end
 
   it "can get summoners whose names have whitespace" do
     summoner = Vigor.summoner("Best Riven NA")
-    summoner.id.should == 32400810
-    summoner.name.should == "Best Riven NA"
-    summoner.level.should == 30
+    expect(summoner.id).to eq 32400810
+    expect(summoner.name).to eq "Best Riven NA"
+    expect(summoner.level).to eq 30
   end
 
   it "can get all champions" do
     champs = Vigor.all_champions
-    champs.length.should == 117
+    expect(champs.length).to eq 117
     zyra = champs.last
 
-    zyra.name.should == "Zyra"
-    zyra.id.should == 143
+    expect(zyra.name).to eq "Zyra"
+    expect(zyra.id).to eq 143
 
-    zyra.should be_in_ranked
-    zyra.should be_active
-    zyra.has_bot?.should be_false
-    zyra.has_mm_bot?.should be_true
-    zyra.should_not be_free_to_play
+    expect(zyra).to be_in_ranked
+    expect(zyra).to be_active
+    expect(zyra.has_bot?).to be false
+    expect(zyra.has_mm_bot?).to be true
+    expect(zyra).not_to be_free_to_play
 
-    zyra.defense.should == 3
-    zyra.attack.should == 4
-    zyra.difficulty.should == 7
-    zyra.magic.should == 8
+    expect(zyra.defense).to eq 3
+    expect(zyra.attack).to eq 4
+    expect(zyra.difficulty).to eq 7
+    expect(zyra.magic).to eq 8
   end
 
   it "can get a champion by name" do
     zyra = Vigor.champion("Zyra")
-    zyra.id.should == 143
+    expect(zyra.id).to eq 143
   end
 
   it "can get free-to-play champions" do
     free_champs = Vigor.free_to_play
-    free_champs.length.should == 10
+    expect(free_champs.length).to eq 10
   end
 
   it "can get recent games" do
     recent_games = Vigor.summoner("Semiel").recent_games
-    recent_games.length.should == 10
+    expect(recent_games.length).to eq 10
 
     most_recent = recent_games.first
-    most_recent.level.should == 30
-    most_recent.mode.should == "ARAM"
-    most_recent.type.should == "MATCHED_GAME"
-    most_recent.should_not be_invalid
-    most_recent.map.should == 12
-    most_recent.spells.should == [4, 7]
+    expect(most_recent.level).to eq 30
+    expect(most_recent.mode).to eq "ARAM"
+    expect(most_recent.type).to eq "MATCHED_GAME"
+    expect(most_recent).to_not be_invalid
+    expect(most_recent.map).to eq 12
+    expect(most_recent.spells).to eq [4, 7]
 
     double_kills = most_recent.stats.find {|stat| stat["id"] == 16 }
-    double_kills["value"].should == 1
+    expect(double_kills["value"]).to eq 1
 
     players = most_recent.fellow_players
-    players.length.should == 9
-    players.first.champion_id.should == 33
+    expect(players.length).to eq 9
+    expect(players.first.champion_id).to eq 33
   end
 end
