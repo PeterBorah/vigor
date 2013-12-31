184767b4cd6ff46e450268a974b25becd35bc9e9
Jade McGough <jade@thezets.com>
2013-12-31 04:17:23 -0800

    refactoring vigor.rb

--- lib/vigor.rb
+++ lib/vigor.rb
@@ -13,66 +13,67 @@ require_relative 'vigor/game'
 require_relative 'vigor/team.rb'
 
 class Vigor
+  class << self
+    @@regions = ['na', 'euw', 'eune', 'br', 'tr']
 
-  @@regions = ['na', 'euw', 'eune', 'br', 'tr']
-
-  def self.configure(api_key, region = "na")
-    region.downcase!
-    raise Vigor::Error::InvalidRegion, "Invalid Region Configuration" unless @@regions.include?(region)
-    Client.default_params :api_key => api_key
-    Client.base_uri "http://prod.api.pvp.net/api/lol/#{region}"
-    self
-  end
-
-  def self.summoner(lookup_value)
-    if lookup_value.is_a? String
-      return Summoner.new(Client.get("/v1.2/summoner/by-name/" + lookup_value.gsub(/\s+/, "")))
-    else
-      return Summoner.new(Client.get("/v1.2/summoner/" + lookup_value.to_s))
+    def configure(api_key, region = "na")
+      region.downcase!
+      raise Vigor::Error::InvalidRegion, "Invalid Region Configuration" unless @@regions.include?(region)
+      Client.default_params :api_key => api_key
+      Client.base_uri "http://prod.api.pvp.net/api/lol/#{region}"
+      self
     end
-  end
 
-  def self.summoners(ids)
-    if ids.is_a? String
-      response = Client.get("/v1.2/summoner/#{URI.encode(ids)}/name")
-    elsif ids.is_a? Array
-      response = Client.get("/v1.2/summoner/#{URI.encode(ids.join(", "))}/name")
-    else
-      return []
+    def summoner(lookup_value)
+      if lookup_value.is_a? String
+        return Summoner.new(Client.get("/v1.2/summoner/by-name/" + lookup_value.gsub(/\s+/, "")))
+      else
+        return Summoner.new(Client.get("/v1.2/summoner/" + lookup_value.to_s))
+      end
     end
-    return response["summoners"].map { |summoner| Summoner.new(summoner, :name_and_id) }
-  end
 
-  def self.mastery_pages(id)
-    Client.get("/v1.2/summoner/#{id.to_s}/masteries")["pages"].map {|page| MasteryPage.new(page)}
-  end
-
-  def self.rune_pages(id)
-    Client.get("/v1.2/summoner/#{id.to_s}/runes")["pages"].map {|page| RunePage.new(page)}
-  end
-
-  def self.all_champions
-    @@champions ||= Client.get("/v1.1/champion")["champions"].map {|champ| Champion.new(champ)}
-  end
-
-  def self.free_to_play
-    Client.get("/v1.1/champion/", query: {"freeToPlay" => true})["champions"].map {|champ| Champion.new(champ)}
-  end
-
-  def self.champion(lookup_value)
-    if lookup_value.is_a? String
-      self.all_champions.find {|champ| champ.name == lookup_value}
-    else
-      self.all_champions.find {|champ| champ.id == lookup_value}
+    def summoners(ids)
+      if ids.is_a? String
+        response = Client.get("/v1.2/summoner/#{URI.encode(ids)}/name")
+      elsif ids.is_a? Array
+        response = Client.get("/v1.2/summoner/#{URI.encode(ids.join(", "))}/name")
+      else
+        return []
+      end
+      return response["summoners"].map { |summoner| Summoner.new(summoner, :name_and_id) }
     end
-  end
 
-  def self.recent_games(id)
-    games = Client.get("/v1.2/game/by-summoner/#{id.to_s}/recent")["games"].map {|game| Game.new(game)}
-    games.sort { |x, y| y.created_at <=> x.created_at }
-  end
+    def mastery_pages(id)
+      Client.get("/v1.2/summoner/#{id.to_s}/masteries")["pages"].map {|page| MasteryPage.new(page)}
+    end
 
-  def self.teams(id)
-    Client.get("/v2.2/team/by-summoner/#{id.to_s}").map {|team| Team.new(team)}
+    def rune_pages(id)
+      Client.get("/v1.2/summoner/#{id.to_s}/runes")["pages"].map {|page| RunePage.new(page)}
+    end
+
+    def all_champions
+      @@champions ||= Client.get("/v1.1/champion")["champions"].map {|champ| Champion.new(champ)}
+    end
+
+    def free_to_play
+      Client.get("/v1.1/champion/", query: {"freeToPlay" => true})["champions"].map {|champ| Champion.new(champ)}
+    end
+
+    def champion(lookup_value)
+      if lookup_value.is_a? String
+        self.all_champions.find {|champ| champ.name == lookup_value}
+      else
+        self.all_champions.find {|champ| champ.id == lookup_value}
+      end
+    end
+
+    def recent_games(id)
+      games = Client.get("/v1.2/game/by-summoner/#{id.to_s}/recent")["games"].map {|game| Game.new(game)}
+      games.sort { |x, y| y.created_at <=> x.created_at }
+    end
+
+    def teams(id)
+      Client.get("/v2.2/team/by-summoner/#{id.to_s}").map {|team| Team.new(team)}
+    end
   end
 end
