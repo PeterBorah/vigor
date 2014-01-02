7e30b471a4f4527bede98e2e272e274fffb9dcc6
Jade McGough <jade@thezets.com>
2014-01-01 19:30:49 -0800

    Added Enumerable and [] to ChampionStats

--- lib/vigor/champion_stats.rb
+++ lib/vigor/champion_stats.rb
@@ -1,12 +1,30 @@
 class Vigor
   class ChampionStats
     attr_accessor :champions, :modify_date
+    include Enumerable
 
     def initialize(data)
       @champions = data['champions'].map { |champion| ChampionAggregate.new(champion) }
       @modify_date = DateTime.strptime((data['modifyDate']/1000).to_s, '%s')
     end
 
+    def combined
+      @champions.last.stats
+    end
+
+    def each(&block)
+      @champions.each do |champion|
+        block.call(champion)
+      end
+    end
+
+    def [](key)
+      if key.kind_of?(Integer)
+        return @champions[key]
+      else
+        return (@champions.find { |champ| champ.name.casecmp(key) == 0 }).stats
+      end
+    end
   end
 
   class ChampionAggregate # rename this, tired atm ><
