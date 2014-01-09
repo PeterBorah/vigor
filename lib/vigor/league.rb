97bd0fbb684c888a602ad83c7efa288366c5c54f
Jade McGough <jade@thezets.com>
2014-01-09 15:05:18 -0800

    Moved League classes into Vigor class (oops), added missing data and accessors, added LeagueItem#to_summoner

--- lib/vigor/league.rb
+++ lib/vigor/league.rb
@@ -11,56 +11,61 @@ class Vigor
     end
 
   end
-end
 
-#todo - split this into LeagueSummoner and LeagueTeam - then add to_team and to_summoner
+  class LeagueItem
+    attr_accessor :promo_series, :last_played, :name, :id, :rank, :tier, :wins, :queue_type
 
-class LeagueItem
-  attr_accessor :promo_series, :last_played
+    def initialize(data)
+      @is_fresh_blood = data['isFreshBlood']
+      @is_hot_streak = data['isHotStreak']
+      @is_inactive = data['isInactive']
+      @is_veteran = data['isVeteran']
+      @last_played = data['lastPlayed']
+      @promo_series = PromoSeries.new(data['miniseries']) unless !data.include?('miniseries')
+      @id = data['playerOrTeamId']
+      @name = data['playerOrTeamName']
+      @rank = data['rank']
+      @tier = data['tier']
+      @wins = data['wins']
+      @queue_type = data['queueType']
+    end
 
-  def initialize(data)
-    @is_fresh_blood = data['isFreshBlood']
-    @is_hot_streak = data['isHotStreak']
-    @is_inactive = data['isInactive']
-    @is_veteran = data['isVeteran']
-    @last_played = data['lastPlayed']
-    @promo_series = PromoSeries.new(data['miniseries']) unless !data.include?('miniseries')
-    @id = data['playerOrTeamId']
-    @name = data['playerOrTeamName']
-    @rank = data['rank']
-    @tier = data['tier']
-    @wins = data['wins']
+    def is_fresh_blood?
+      @is_fresh_blood
+    end
+
+    def is_hot_streak?
+      @is_hot_streak
+    end
+
+    def is_inactive?
+      @is_inactive
+    end
+
+    def is_veteran?
+      @is_vetaran
+    end
+
+    def in_series?
+      !@promo_series.nil?
+    end
+
+    def to_summoner
+      return nil unless @queue_type == "RANKED_SOLO_5x5"
+      return Summoner.new({"id" => @id, "name" => @name}, :name_and_id)
+    end
+
   end
 
-  def is_fresh_blood?
-    @is_fresh_blood
+  class PromoSeries
+    attr_accessor :losses, :progress, :target, :time_left
+
+    def initialize(data)
+      @losses = data['losses']
+      @progress = data['progress']
+      @target = data['target']
+      @time_left = data['timeLeftToPlayMillis']
+    end
+
   end
-
-  def is_hot_streak?
-    @is_hot_streak
-  end
-
-  def is_inactive?
-    @is_inactive
-  end
-
-  def is_veteran?
-    @is_vetaran
-  end
-
-  def in_series?
-    !@promo_series.nil?
-  end
-
-end
-
-class PromoSeries
-
-  def initialize(data)
-    @losses = data['losses']
-    @progress = data['progress']
-    @target = data['target']
-    @timeLeft = data['timeLeftToPlayMillis']
-  end
-
 end
